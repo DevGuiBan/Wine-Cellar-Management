c9595b77b4e3ea9d2374de5bc0b6038e63a929bd
Guilherme Bandeira <107276182+DevGuiBan@users.noreply.github.com>
2024-11-19 15:58:54 -0300

    Add files via upload

---
+++ src/test/java/com/ggnarp/winecellarmanagement/WineCellarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggnarp.winecellarmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineCellarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
