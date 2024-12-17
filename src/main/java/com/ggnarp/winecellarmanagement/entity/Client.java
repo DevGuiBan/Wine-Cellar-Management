3c94db2ee749d1eb52d989a4cb9c75b162290f06
DevGuiBan <guibandeira290@gmail.com>
2024-12-17 14:57:19 -0300

    add supplier

--- src/main/java/com/ggnarp/winecellarmanagement/entity/Client.java
+++ src/main/java/com/ggnarp/winecellarmanagement/entity/Client.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "clientes")
+@Table(name = "client")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
---
+++ src/main/resources/db/migration/V3__create_table_supplier.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXIST supplier {
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    cnpj VARCHAR(255),
+    email VARCHAR(255)
+    phone_number VARCHAR(255),
+    address VARCHAR(255)
+}
